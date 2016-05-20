b58fa1f7865f1351f8dd368dc52a8a753fa5670e
jonathan <jonathan.garrido1988@gmail.com>
2016-05-20 18:59:10 -0300

    problemas con una animación side 11, lo demás listo

--- app/js/app.js
+++ app/js/app.js
@@ -9,6 +9,10 @@
   };
   fullHome();
 
+  var ancho = window.innerWidth;
+  console.log(ancho);
+  
+
   $(window).scroll(function(){
 
     var wScroll = $(this).scrollTop();
@@ -44,6 +48,78 @@
       $('.slide6-3-left').removeClass('slide-left');
       $('.slide6-3-right').removeClass('slide-right');
     }
+
+    if(wScroll > $('.slide8').offset().top + 200){
+      $('.slide8-1').addClass('fade-in');
+      $('.slide8').addClass('bg-slide8');
+    }else{
+      $('.slide8-1').removeClass('fade-in');
+      $('.slide8').removeClass('bg-slide8');
+      
+    }
+
+    // POSITION FIXED 
+    if(wScroll > $('.slide7').offset().top){
+      $('.slide8').addClass('zIndex');
+    }else{
+      $('.slide8').removeClass('zIndex');
+    }
+    if(wScroll > $('.slide9').offset().top){
+      $('.slide8').removeClass('zIndex');
+    }
+
+
+    if(wScroll > $('.slide10').offset().top){
+      $('.slide10-dual').addClass('zIndex');
+    }else{
+      $('.slide10-dual').removeClass('zIndex');
+    }
+    if(wScroll > $('.slide10-fin').offset().top){
+      $('.slide10-dual').removeClass('zIndex');
+    }
+
+    if(ancho>=720) {
+      var anchoimg = $('#punto').height();
+      var posit =  52 + anchoimg;
+    
+      if(wScroll > $('.slide10-fin').offset().top){
+        $('.slide11').addClass('zIndex');
+      }else{
+        $('.slide11').removeClass('zIndex');
+      }
+
+      if(wScroll > $('.slide11').offset().top + 115){
+        $('.slide11-principal').addClass('stiky');
+      }else{
+        $('.slide11-principal').removeClass('stiky');
+      }
+
+      if(wScroll > $('.slide11').offset().top + 520){
+        $('.slide11-txt-1').addClass('stiky2');
+        $('.slide11-txt-1').css('top',posit);
+      }else{
+        $('.slide11-txt-1').removeClass('stiky2');
+        $('.slide11-txt-1').css('top',0);
+      }
+
+
+      if(wScroll > $('.slide11').offset().top + 990){
+        $('.slide11-txt-2').addClass('stiky2');
+        $('.slide11-txt-2').css('top',posit);
+      }else{
+        $('.slide11-txt-2').removeClass('stiky2');
+        $('.slide11-txt-2').css('top',0);
+      }
+
+      if(wScroll > $('.slide11').offset().top + 1420){
+        $('.slide11-txt-3').addClass('stiky2');
+        $('.slide11-txt-3').css('top',posit);
+      }else{
+        $('.slide11-txt-3').removeClass('stiky2');
+        $('.slide11-txt-3').css('top',0);
+      }
+    }
+    
 
     
   });
